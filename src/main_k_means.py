99ee49f837e1b19544482842dca2b417ba4cfa2c
omid sotooni <mr.sotooni@yahoo.com>
2022-03-04 19:44:18 +0330

    use libs for show picture 2D and add the codes

--- src/main_k_means.py
+++ src/main_k_means.py
@@ -2,6 +2,9 @@ import sys
 from LoadData import * 
 from k_means import * 
 from evaluation import * 
+from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -19,13 +22,14 @@ if __name__ == "__main__":
     K = 3  # Suppose there are 2 clusters
     print ('K=',K)
 
+    # انتخاب دو عدد اول به عنوان مرکز خوشه
     # use the first two data points as initial cluster centers
     centers = []
     for i in range(K):
         centers.append(data[i])
 
 
-    # get clusterID, list
+    # get clusterID, list, updated centers
     results = kmeans(data, centers) 
 
     res_Purity = purity(groundtruth, results) 
@@ -34,4 +38,30 @@ if __name__ == "__main__":
     print ("Purity =", res_Purity)
     print ("NMI = ", res_NMI)
     
+
+    # برای اینکه راحت تر ایندکس بشه نقاط دوباره تعیین میشوند
+    points = np.empty((0,len(data[0])), float)
+    # محاسبه ی فاصله که برای پیدا کردن داده های پرت استفاده میشود
+    distances = np.empty((0,len(data[0])), float)
+    
+    d={}
+    for x,y in zip(data,results):
+        d.setdefault(y, []).append(x)
+    
+    for i, center_elem in enumerate(centers):
+        # cdist از این کتابخانه برای حساب کردن فاصله هر داده تا مرکز استفاده میشود
+        distances = np.append(distances , cdist([center_elem],d[i], 'euclidean')) 
+        points = np.append(points, data, axis=0)
+    print(points)
+    percentile = 98
+    # داده های پرتی که فاصله آنها از مرکز بیشتر از درصد تعیین شده را پیدا میکند
+    outliers = points[np.where(distances > np.percentile(distances, percentile))]
+    fig = plt.figure()    
+    plt.scatter(*zip(*data),c=results,marker='s',facecolor="g" ) 
+    # یک ضربدر روی داده های پرت میزند
+    plt.scatter(*zip(*outliers),marker="x",edgecolor="b",s=70);
+    # مرکز هر خوشه یک دایره با کادر قرمز نمایش داده میشود
+    plt.scatter(*zip(*centers),marker="o",edgecolor="r",s=20);   
+    plt.savefig() 
+    plt.show()
 
